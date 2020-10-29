c82f1dd07e43dafdd6b7ba0002273ba859358a06
devBublik <ew.sorokina2011@yandex.ru>
2020-10-29 20:22:14 +0300

    start header circle

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,33 @@
-$(function() {
+$(function () {
 
-	$(".rate-star").rateYo({
-		rating: 5,
-		starWidth: '12px',
-		readOnly: true
-	});
-
-	$(".js-range-slider").ionRangeSlider({
-		type: "double",
-        
-        min: 0,
-        max: 1000,
-        from: 0,
-        to: 600,
-        prefix: "$"
-});
 
 
 	var mixer = mixitup('.product__inner-box');
-	
+
+	// $(".rate-star").rateYo({
+	// 	rating: 5,
+	// 	starWidth: '12px',
+	// 	readOnly: true
+	// });
+
+
 	$('.product-slider-inner').slick({
-		dots: true, 
+		dots: true,
 		arrows: false,
 		slidesToShow: 4,
 		slidesToScroll: 4
 	});
+
+	$(".js-range-slider").ionRangeSlider({
+		type: "double",
+		min: 0,
+		max: 1000,
+		from: 0,
+		to: 600,
+		prefix: "$"
+	});
+
+
 
 
 
